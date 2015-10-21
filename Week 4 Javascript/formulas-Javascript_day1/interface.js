c07a4314c116c3725e501b76bbe5bf057d1820f2
carlavenini <carlavenini@kink.com>
2015-10-21 16:49:36 -0700

    new background image

---
+++ Week 4 Javascript/formulas-Javascript_day1/interface.js
@@ -0,0 +1,69 @@
+var pyA = document.querySelector('.py-a');
+var pyB = document.querySelector('.py-b');
+var pyC = document.querySelector('.py-c');
+
+pyA.addEventListener('keyup', calcPy);
+pyB.addEventListener('keyup', calcPy);
+
+function calcPy(event) {
+  if (pyA.value.length > 0 && pyB.value.length > 0) {
+    pyC.value = calcLongestSide(pyA.value, pyB.value)
+  } else {
+    pyC.value = '';
+  }
+}
+
+
+
+
+
+
+var r = document.querySelector('.circum-r');
+var c = document.querySelector('.circum-c');
+
+r.addEventListener('keyup', calcC);
+
+function calcC(event) {
+  if (r.value.length > 0) {
+    c.value = 3.14 * (r.value * r.value);
+
+}
+}
+
+
+
+
+
+
+var tempF = document.querySelector('.temp-f');
+var tempC = document.querySelector('.temp-c');
+
+tempF.addEventListener('keyup', calcFtoC);
+tempC.addEventListener('keyup', calcCtoF);
+
+function calcFtoC(event) {
+  if (tempF.value.length > 0) {
+    tempC.value = calcFahrenheitToCelcius(tempF.value)
+  } else {
+    tempC.value = '';
+  }
+}
+
+function calcFahrenheitToCelcius (fahrenheit){
+  var celsius = (fahrenheit - 32) * 5/9;
+        return celsius;
+
+}
+
+function calcCelsToFahr (celsius) {
+        var farh = celsius * (9/5) + 32;
+        return farh;
+}
+
+function calcCtoF(event) {
+  if (tempC.value.length > 0) {
+    tempF.value = calcCelciusToFahrenheit(tempC.value)
+  } else {
+    tempF.value = '';
+  }
+}
