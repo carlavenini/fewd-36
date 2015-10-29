d363b20f7f643d5887f600607b7a553a342b1b18
carlavenini <carlavenini@kink.com>
2015-10-28 18:29:59 -0700

    Compare 2 Numbers-homework

---
+++ Week 5/compare-two-numbers/js/compare.js
@@ -0,0 +1,35 @@
+	var firstNumber = document.querySelector(".firstNumber");
+	var secondNumber = document.querySelector(".secondNumebr");
+
+
+
+function changeValue() {
+   var firstNumber = event.target;
+   var secondNumber = swatch.className;
+   
+   var body = document.querySelector("body");
+   body.className = color;
+
+   var name = document.querySelector("span");
+   name.textContent = color;
+
+
+   <button class="compare">Compare</button>
+
+   // conditionals 
+   // log greater
+   // Eg: if num1 > num2
+   // log less
+   // if else num1 > num2
+
+   // 	else
+   // 		console log =
+
+   var sign.textContent = 
+
+   sign.textContent <
+
+   sign.textContent >
+   update sign.textContent = x;
+
+   sign.textContent
