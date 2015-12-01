24db460b79c43c74295e19b57c16e510b33def23
carlavenini <carlavenini@kink.com>
2015-11-30 20:37:33 -0800

    Final Project edits
    added an HOURS page

--- FINAL PROJECT/FINALFEWD/js/script.js
+++ FINAL PROJECT/FINALFEWD/js/script.js
@@ -1,10 +1,12 @@
 console.log("this works");
  
-// Setting up the global variables
+// Setting up the global variables (grabbed elements from the DOM)
  var chocolate= document.getElementById("chocolate");
  var run= document.getElementById("run");
  var alcohol= document.getElementById("alcohol");
- var sex= document.getElementById("sex");
+ var orgasm= document.getElementById("orgasm");
+ var levelSpike = document.getElementById("levelSpike");
+ var stimulusInfo = document.getElementById("stimulusInfo");
  // var run = 
 
 
@@ -13,24 +15,73 @@ console.log("this works");
  // Stimuli Variables
 
  var chocolate_text = "Chocolate tastes great";
+ var chocolateStimulus = "more information about DARK CHOCOLATE and how it affects you";
+ var chocolateLevel = "100%"
+ 
+
+
  var run_text = "Running is great";
+ var runStimulus = "more information about RUNNING and how it affects you";
+ var runLevel = "325%"
+
+
+
  var alcohol_text = "Alcohol is great for you";
- var sex_text = "Sex is great for you";
+ var alcoholStimulus = "more information about ALCOHOL and how it affects you";
+ var alcoholLevel = "125%"
+
+
+
+ var orgasm_text = "Sex is great for you";
+ var orgasmStimulus = "more information about ORGASMS and how they affect you";
+ var orgasmLevel = "250%"
+
+ 
 
  // Adding Event Listeners
 
- chocolate.addEventListener("mouseover", changeText);
- run.addEventListener("mouseover", changeText);
- alcohol.addEventListener("mouseover", changeText);
- sex.addEventListener("mouseover", changeText);
+ chocolate.addEventListener("mouseover", changeChocolate);
+ run.addEventListener("mouseover", changeRun);
+ alcohol.addEventListener("mouseover", changeAlcohol);
+ orgasm.addEventListener("mouseover", changeOrgasm);
 
 
  // Functions
- function changeText(e){
+ function changeChocolate(e){
  		e.preventDefault();
- 		information.textContent  = chocolate_text;
- 		information.textContent  = run_text;
- 		information.textContent  = alcohol_text;
- 		information.textContent  = sex_text;
+ 		information.textContent = chocolate_text;
+ 		stimulusInfo.textContent = chocolateStimulus;
+ 		levelSpike.textContent = chocolateLevel;
 
- }
+ 		}
+
+function changeRun(e){
+ 		e.preventDefault();
+ 		information.textContent = run_text;
+ 		stimulusInfo.textContent = runStimulus;
+ 		levelSpike.textContent = runLevel;
+ 	}
+
+function changeAlcohol(e){
+ 		e.preventDefault();
+ 		information.textContent = alcohol_text;
+ 		stimulusInfo.textContent = alcoholStimulus;
+ 		levelSpike.textContent = alcoholLevel;
+ 	}
+
+function changeOrgasm(e){
+ 		e.preventDefault();
+ 		information.textContent = orgasm_text;
+ 		stimulusInfo.textContent = orgasmStimulus;
+ 		levelSpike.textContent = orgasmLevel;
+ 	}
+
+
+ 		// information.textContent  = chocolate_text;
+ 		// information.textContent  = run_text;
+ 		// information.textContent  = alcohol_text;
+ 		// information.textContent  = sex_text;
+
+
+
+
